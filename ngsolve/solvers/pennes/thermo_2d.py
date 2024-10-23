08f7168e4774d4cc07409f7a495fb91f97f0c33a
Andriy <Andriy@noemail.com>
2024-10-23 18:24:21 +0000

    Added data

--- ngsolve/solvers/pennes/thermo_2d.py
+++ ngsolve/solvers/pennes/thermo_2d.py
@@ -1,10 +1,23 @@
 from ngsolve import *
 from netgen.occ import *
 
-PARAM_A = 0.08
-PARAM_B = 0.16
-PARAM_C = 0.4
-OMEGA3_RADIUS = 0.005
+PARAM_A = 0.08 # m
+PARAM_B = 0.16 # m
+PARAM_C = 0.4 # m
+
+OMEGA3_RADIUS = 0.005 # m
+OMEGA3_CENTER_X = PARAM_C / 2.0
+OMEGA3_CENTER_Y = PARAM_A - OMEGA3_RADIUS
+
+K_TISSUE = 0.19 # W/m/K ==  W/m/Celcius
+K_MAX_TUMOR = 0.495 # W/m/Celcius
+K_CO2 = 0.0176 # W/m/Celcius
+
+PERFUSION_OMEGA = 1.5 / 1000.0 / 60.0 * 10.0 # l/s/kg
+BLOOD_DENSITY = 1050 # kg/m**3
+BLOOD_HEAT_CAPACITY = 3617 # J/kg/Celcius
+BODY_TEMP = 37 # Celcius
+TISSUE_HEAT_GENERATION = 1.2 * 1086  # W/m**3, based on data Urinary Bladder Wall 1.21 W/kg, density 1086 kg/m**3
 
 BORDER_GAMMA1 = "gamma1"
 BORDER_GAMMA2 = "gamma2"
@@ -20,7 +33,7 @@ SMALL_MAXH = 0.05
 whole_box = Rectangle(PARAM_C, PARAM_A + PARAM_B).Face()
 bottom_box = Rectangle(PARAM_C, PARAM_A).Face()
 
-tumor_domain = Circle((PARAM_C / 2, PARAM_A - OMEGA3_RADIUS), OMEGA3_RADIUS).Face()
+tumor_domain = Circle((OMEGA3_CENTER_X, OMEGA3_CENTER_Y), OMEGA3_RADIUS).Face()
 tissue_domain = bottom_box - tumor_domain
 co2_domain = whole_box - bottom_box  
 
@@ -51,6 +64,14 @@ u = fes.TrialFunction()
 v = fes.TestFunction()
 
 gfu = GridFunction(fes)
+
+thermal_conductivity = mesh.MaterialCF({
+    DOMAIN_TISSUE: K_TISSUE,
+    DOMAIN_TUMOR: (K_MAX_TUMOR - K_TISSUE) * cos(0.5 * pi / OMEGA3_RADIUS ** 2 * (x - OMEGA3_CENTER_X) ** 2 + 
+                                                 (y - OMEGA3_CENTER_Y) ** 2) + K_TISSUE,
+    DOMAIN_CO2: K_CO2
+    }, 
+    default = 0) 
 
 heat_source = mesh.MaterialCF({ DOMAIN_TISSUE : 0.01 - (x - PARAM_C / 2) * (x - PARAM_C / 2) + (y - PARAM_A / 2) * (y - PARAM_A / 2) }, 
                               default = 0)
