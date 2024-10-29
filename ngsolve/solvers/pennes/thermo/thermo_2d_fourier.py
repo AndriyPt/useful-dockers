62bf75dd9392afbc012d7f76ed3286e474fa7d93
Andriy <Andriy@noemail.com>
2024-10-29 22:50:11 +0000

    Added extra border conditions

---
+++ ngsolve/solvers/pennes/thermo/thermo_2d_fourier.py
@@ -0,0 +1,118 @@
+from ngsolve import *
+from netgen.occ import *
+
+PARAM_A = 0.08 # m
+PARAM_B = 0.16 # m
+PARAM_C = 0.4 # m
+
+OMEGA3_RADIUS = 0.005 # m
+OMEGA3_CENTER_X = PARAM_C / 2.0
+OMEGA3_CENTER_Y = PARAM_A - OMEGA3_RADIUS
+
+K_TISSUE = 0.19 # W/m/K ==  W/m/Celcius
+K_MAX_TUMOR = 0.495 # W/m/Celcius
+K_CO2 = 0.0176 # W/m/Celcius
+
+PERFUSION_OMEGA = 1.5 / 1000.0 / 60.0 * 10.0 # l/s/kg
+BLOOD_DENSITY = 1050 # kg/m**3
+BLOOD_HEAT_CAPACITY = 3617 # J/kg/Celcius
+BODY_TEMP = 37 # Celcius
+TISSUE_HEAT_GENERATION = 1.2 * 1086  # W/m**3, based on data Urinary Bladder Wall 1.21 W/kg, density 1086 kg/m**3
+
+BORDER_GAMMA1 = "gamma1"
+BORDER_GAMMA2 = "gamma2"
+BORDER_CONTACT = "contact"
+
+DOMAIN_TISSUE = "tissue"
+DOMAIN_CO2 = "co2"
+DOMAIN_TUMOR = "tumor"
+
+ROBIN_BC_APLHA = 5
+ROBIN_BC_ROOM_TEMP = 23
+
+LARGE_MAXH = 0.1
+SMALL_MAXH = 0.05
+
+whole_box = Rectangle(PARAM_C, PARAM_A + PARAM_B).Face()
+bottom_box = Rectangle(PARAM_C, PARAM_A).Face()
+
+tumor_domain = Circle((OMEGA3_CENTER_X, OMEGA3_CENTER_Y), OMEGA3_RADIUS).Face()
+tissue_domain = bottom_box - tumor_domain
+co2_domain = whole_box - bottom_box  
+
+tumor_domain.faces.name = DOMAIN_TUMOR
+tissue_domain.faces.name = DOMAIN_TISSUE
+co2_domain.faces.name = DOMAIN_CO2
+
+tissue_domain.edges.Min(X).name = BORDER_GAMMA2
+tissue_domain.edges.Min(Y).name = BORDER_GAMMA1
+tissue_domain.edges.Max(X).name = BORDER_GAMMA2
+
+co2_domain.edges.Max(Y).name = BORDER_GAMMA2
+co2_domain.edges.Min(X).name = BORDER_GAMMA2
+co2_domain.edges.Max(X).name = BORDER_GAMMA2
+
+shape = Glue([tumor_domain, tissue_domain, co2_domain])
+
+geo = OCCGeometry(shape, dim = 2)
+mesh = Mesh(geo.GenerateMesh(maxh=LARGE_MAXH)).Curve(3)
+
+print("Boundaries: ", mesh.GetBoundaries())
+print("Materials: ", mesh.GetMaterials())
+
+Draw(mesh)
+
+fes = H1(mesh, order=3, dirichlet=BORDER_GAMMA1)
+u = fes.TrialFunction()
+v = fes.TestFunction()
+
+# Dirichlet conditions
+dirichlet_conditions = mesh.BoundaryCF({BORDER_GAMMA1: BODY_TEMP}, default = 0)
+gfu = GridFunction(fes)
+gfu.Set(dirichlet_conditions, BND)
+
+Draw(gfu, mesh, "Dirichlet substitution")
+
+thermal_conductivity = mesh.MaterialCF({
+    DOMAIN_TISSUE: K_TISSUE,
+    DOMAIN_TUMOR: (K_MAX_TUMOR - K_TISSUE) * cos(0.5 * pi / OMEGA3_RADIUS ** 2 * ((x - OMEGA3_CENTER_X) ** 2 + 
+                                                 (y - OMEGA3_CENTER_Y) ** 2)) + K_TISSUE,
+    DOMAIN_CO2: K_CO2
+    }, 
+    default = 0) 
+
+Draw(thermal_conductivity, mesh, "Thermal Conductivity")
+
+heat_source = mesh.MaterialCF({ 
+    DOMAIN_TISSUE: PERFUSION_OMEGA * BLOOD_DENSITY * BLOOD_HEAT_CAPACITY * BODY_TEMP + TISSUE_HEAT_GENERATION,
+    DOMAIN_TUMOR: PERFUSION_OMEGA * BLOOD_DENSITY * BLOOD_HEAT_CAPACITY * BODY_TEMP + TISSUE_HEAT_GENERATION  # Could be OK for small Tumor
+    },
+    default = 0)
+
+Draw(heat_source, mesh, "Heat Source")
+
+u_coeficient = mesh.MaterialCF({ 
+    DOMAIN_TISSUE: -1.0 * PERFUSION_OMEGA * BLOOD_DENSITY * BLOOD_HEAT_CAPACITY,
+    DOMAIN_TUMOR: -1.0 * PERFUSION_OMEGA * BLOOD_DENSITY * BLOOD_HEAT_CAPACITY  # Could be OK for small Tumor
+    },
+    default = 0)
+
+Draw(u_coeficient, mesh, "u Coefficient")
+
+a = BilinearForm(thermal_conductivity * grad(u) * grad(v) * dx + u_coeficient * u * v * dx + ROBIN_BC_APLHA * u * v * ds(BORDER_GAMMA2))
+
+f = LinearForm (heat_source * v * dx + ROBIN_BC_APLHA * (ROBIN_BC_ROOM_TEMP - BODY_TEMP) * v * ds(BORDER_GAMMA2))
+
+f.Assemble()
+a.Assemble()
+
+
+# Approach for nonhomogeneous Dirichlet boundary condition
+# https://docu.ngsolve.org/release/i-tutorials/unit-1.3-dirichlet/dirichlet.html
+r = f.vec.CreateVector()
+r.data = f.vec - a.mat * gfu.vec
+
+# the solution field 
+gfu.vec.data += a.mat.Inverse(freedofs=fes.FreeDofs()) * r
+
+Draw(gfu, mesh, "Heat")
