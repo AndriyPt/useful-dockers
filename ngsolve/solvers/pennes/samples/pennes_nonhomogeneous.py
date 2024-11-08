8a04c042dbbc37ecf0bfb97a2ead7492a90ff5e1
Andriy <Andriy@noemail.com>
2024-11-08 14:32:51 +0000

    Added Pennes equation solution for nonhomogeneous materials

--- ngsolve/solvers/pennes/samples/pennes_nonhomogeneous.py
+++ ngsolve/solvers/pennes/samples/pennes_nonhomogeneous.py
@@ -1,12 +1,12 @@
 from ngsolve import *
 from netgen.occ import *
 
-RADIUS = 0.045 # m
 
 ROBIN_H = 4.184 # W/m^2/^C
 ROBIN_T_e = 12 # ^C
 
-THERMAL_K = 0.6272 # W/m/^C
+THERMAL_K_tis = 0.19 # W/m/^C
+THERMAL_K_tum = 0.495 # W/m/^C
 
 THERMAL_Q_m = 418.4 # W/m^3
 THERMAL_T_a = 36.8 # ^C  
@@ -18,20 +18,42 @@ THERMAL_W = DENSITY_B * THERMAL_C_b * THERMAL_omega
 
 BORDER_GAMMA = "gamma"
 DOMAIN_TISSUE = "tissue"
+DOMAIN_TUMOR = "tumor"
 
-LARGE_MAXH=0.005
+PARAM_C = 0.04 # width
+PARAM_A = 0.04 # height
 
-ngsglobals.msg_level = 1
+OMEGA3_RADIUS = 0.005 # m
+OMEGA3_CENTER_X = PARAM_C / 2.0
+OMEGA3_CENTER_Y = PARAM_A - OMEGA3_RADIUS
+
+LARGE_MAXH=0.0025
 
 # generate a triangular mesh
-arm_cross_section = Circle((0,0), RADIUS).Face()
-arm_cross_section.edges.name = BORDER_GAMMA
-arm_cross_section.faces.name = DOMAIN_TISSUE
+whole_tissue = Rectangle(PARAM_C, PARAM_A).Face()
+whole_tissue.edges.name = BORDER_GAMMA
 
-shape = Glue([arm_cross_section])
+tumor_cross_section = Circle((OMEGA3_CENTER_X, OMEGA3_CENTER_Y), OMEGA3_RADIUS).Face()
+tumor_cross_section.faces.name = DOMAIN_TUMOR
+
+tissue_cross_section = whole_tissue - tumor_cross_section 
+tissue_cross_section.faces.name = DOMAIN_TISSUE
+
+shape = Glue([tissue_cross_section, tumor_cross_section])
 
 geo = OCCGeometry(shape, dim = 2)
 mesh = Mesh(geo.GenerateMesh(maxh=LARGE_MAXH)).Curve(3)
+
+print("Boundaries: ", mesh.GetBoundaries())
+print("Materials: ", mesh.GetMaterials())
+
+Draw(mesh)
+
+thermal_conductivity = mesh.MaterialCF({
+    DOMAIN_TISSUE: THERMAL_K_tis,
+    DOMAIN_TUMOR: THERMAL_K_tum,
+    }, 
+    default = 0) 
 
 # H1-conforming finite element space
 fes = H1(mesh, order=5)
@@ -46,7 +68,7 @@ f += (THERMAL_W * THERMAL_T_a + THERMAL_Q_m) * v * dx + ROBIN_H * ROBIN_T_e * v 
 
 # the bilinear-form 
 a = BilinearForm(fes, symmetric=True)
-a += (THERMAL_K * grad(u) * grad(v) + THERMAL_W * u * v) * dx + ROBIN_H * u * v * ds(BORDER_GAMMA) 
+a += (thermal_conductivity * grad(u) * grad(v) + THERMAL_W * u * v) * dx + ROBIN_H * u * v * ds(BORDER_GAMMA) 
 
 a.Assemble()
 f.Assemble()
