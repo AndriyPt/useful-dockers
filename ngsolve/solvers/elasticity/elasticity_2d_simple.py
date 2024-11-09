8bf556926457ad6af12cb55f006b460951d43e37
Andriy <Andriy@noemail.com>
2024-11-09 21:11:40 +0000

    First calclation of elastcity

--- ngsolve/solvers/elasticity/elasticity_2d_simple.py
+++ ngsolve/solvers/elasticity/elasticity_2d_simple.py
@@ -1,49 +1,91 @@
 from netgen.occ import *
 from ngsolve import *
 
-box = Box((0,0,0), (3,0.6,1))
-box.faces.name="outer"
-cyl = sum( [Cylinder((0.5+i,0,0.5), Y, 0.25,0.8) for i in range(3)] )
-cyl.faces.name="cyl"
-geo = box-cyl
-
-cylboxedges = geo.faces["outer"].edges * geo.faces["cyl"].edges
-cylboxedges.name = "cylbox"
-geo = geo.MakeChamfer(cylboxedges, 0.03)
-
-geo.faces.Min(X).name = "fix"
-geo.faces.Max(X).name = "force"
-
-mesh = Mesh(OCCGeometry(geo).GenerateMesh(maxh=0.1)).Curve(3)
-Draw(mesh)
-
 E, nu = 210, 0.2
 mu  = E / 2 / (1+nu)
 lam = E * nu / ((1+nu)*(1-2*nu))
 
-def Stress(strain):
-    return 2*mu*strain + lam*Trace(strain)*Id(3)   
+BORDER_TOP = "gamma_top"
+BORDER_PRESS = "gamma_pressure"
+BORDER_SIDE = "gamma_side"
+BORDER_BOTTOM = "gamma_bottom"
 
-fes = VectorH1(mesh, order=3, dirichlet="fix")
+DOMAIN_TISSUE = "tissue"
+DOMAIN_TUMOR = "tumor"
+
+PARAM_A = 0.04 # height (m)
+PARAM_B = 0.16 # CO2 area height (m)
+PARAM_C = 0.4 # width (m)
+
+TOOL_RADIUS = 0.0025 # m
+TOOL_DISPLACEMENT = -0.001 # m
+
+OMEGA3_RADIUS = 0.0025 # m
+OMEGA3_CENTER_X = PARAM_C / 2.0
+OMEGA3_CENTER_Y = PARAM_A - OMEGA3_RADIUS
+
+LARGE_MAXH=0.005
+
+# generate a triangular mesh
+left_rect = Rectangle(PARAM_C / 2.0 - TOOL_RADIUS, PARAM_A).Face()
+left_rect.edges.Min(X).name = BORDER_SIDE
+left_rect.edges.Max(Y).name = BORDER_TOP
+left_rect.edges.Min(Y).name = BORDER_BOTTOM
+
+middle_rect = MoveTo(PARAM_C / 2.0 - TOOL_RADIUS, 0).Rectangle(2.0 * TOOL_RADIUS, PARAM_A).Face()
+middle_rect.edges.Max(Y).name = BORDER_PRESS
+middle_rect.edges.Min(Y).name = BORDER_BOTTOM
+
+right_rect = MoveTo(PARAM_C / 2.0 + TOOL_RADIUS, 0).Rectangle(PARAM_C / 2.0 - TOOL_RADIUS, PARAM_A).Face()
+right_rect.edges.Max(X).name = BORDER_SIDE
+right_rect.edges.Max(Y).name = BORDER_TOP
+right_rect.edges.Min(Y).name = BORDER_BOTTOM
+
+shape = Glue([left_rect, middle_rect, right_rect])
+
+geo = OCCGeometry(shape, dim = 2)
+mesh = Mesh(geo.GenerateMesh(maxh=LARGE_MAXH)).Curve(3)
+
+print("Boundaries: ", mesh.GetBoundaries())
+print("Materials: ", mesh.GetMaterials())
+
+Draw(mesh)
+
+def Stress(strain):
+    return 2*mu*strain + lam*Trace(strain)*Id(2)   
+
+fes = VectorH1(mesh, order=3, dirichlet=BORDER_BOTTOM + "|" + BORDER_SIDE + "|" + BORDER_PRESS)
+
+# Dirichlet conditions
+dirichlet_conditions = mesh.BoundaryCF({BORDER_PRESS: (0, TOOL_DISPLACEMENT)}, default = (0,0))
+dirichlet_gfu = GridFunction(fes)
+dirichlet_gfu.Set(dirichlet_conditions, BND)
+
 u,v = fes.TnT()
 gfu = GridFunction(fes)
+
+# Added to create correction vector for Dirichlet condition
+force = CF((1.0,0))
+f = LinearForm(force*v*ds(BORDER_TOP)).Assemble()
 
 with TaskManager():
     a = BilinearForm(InnerProduct(Stress(Sym(Grad(u))), Sym(Grad(v))).Compile()*dx)
     pre = Preconditioner(a, "bddc")
     a.Assemble()
 
-force = CF( (1e-3,0,0) )
-f = LinearForm(force*v*ds("force")).Assemble()
-
 from ngsolve.krylovspace import CGSolver
 inv = CGSolver(a.mat, pre, tol=1e-8)
-gfu.vec.data = inv * f.vec
+
+# Approach for nonhomogeneous Dirichlet boundary condition
+# https://docu.ngsolve.org/release/i-tutorials/unit-1.3-dirichlet/dirichlet.html
+r = f.vec.CreateVector()
+r.data = - inv * dirichlet_gfu.vec
+gfu.vec.data = dirichlet_gfu.vec.data + inv * r
 
 with TaskManager():
     fesstress = MatrixValued(H1(mesh,order=3), symmetric=True)
     gfstress = GridFunction(fesstress)
-    gfstress.Interpolate (Stress(Sym(Grad(gfu))))
+    gfstress.Interpolate(Stress(Sym(Grad(gfu))))
 
 Draw(gfu, mesh, "Deformations")
 
