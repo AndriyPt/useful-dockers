005508d89b139c0a9f7bbcd10931e8129c120e3b
Andriy Petlovanyy <Andriy.Petlovanyy@noemail.com>
2025-03-23 19:40:32 +0000

    Added visualization code

--- ngsolve/solvers/samples/manual/bem_dirichlet_2d.py
+++ ngsolve/solvers/samples/manual/bem_dirichlet_2d.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+CHART_STEPS = 100
 
 class ExpressionTerm:
     pass
 
+
 class BoundaryCondition:
     def __init__(self):
         pass
-    
+
     def value(self, point: np.array):
         raise NotImplementedError("Call to abstract method")
+
 
 class BottomDirichletCondition(BoundaryCondition):
     def __init__(self):
@@ -20,6 +26,7 @@ class BottomDirichletCondition(BoundaryCondition):
     def value(self, point: np.array):
         return 0.0
 
+
 class LeftDirichletCondition(BoundaryCondition):
     def __init__(self):
         super().__init__()
@@ -28,13 +35,15 @@ class LeftDirichletCondition(BoundaryCondition):
     def value(self, point: np.array):
         return 2.0 * point[1]
 
+
 class RightDirichletCondition(BoundaryCondition):
     def __init__(self):
         super().__init__()
-        self.__border_elements = np.array([[1.0, 0.0], [1.0, 1,0]])
+        self.__border_elements = np.array([[1.0, 0.0], [1.0, 1, 0]])
 
     def value(self, point: np.array):
         return 2.0 * point[1]
+
 
 class TopDirichletCondition(BoundaryCondition):
     def __init__(self):
@@ -44,28 +53,37 @@ class TopDirichletCondition(BoundaryCondition):
     def value(self, point: np.array):
         return 2.0
 
+
 class ExpressionTerm:
     pass
+
 
 class SingleLayerBoundaryTerm(ExpressionTerm):
     pass
 
+
 class DoubleLayerBoundaryTerm(ExpressionTerm):
     pass
+
 
 class SingleLayerVolumeTerm(ExpressionTerm):
     pass
 
+
 print("Define boundary conditions...")
 
-boundary_conditions = [BottomDirichletCondition(), LeftDirichletCondition(), RightDirichletCondition(), 
-                       TopDirichletCondition()]
+boundary_conditions = [
+    BottomDirichletCondition(),
+    LeftDirichletCondition(),
+    RightDirichletCondition(),
+    TopDirichletCondition(),
+]
 
 expression = [SingleLayerBoundaryTerm(), DoubleLayerBoundaryTerm(), SingleLayerVolumeTerm()]
 
 unknown_count = 10
 
-print("Create SLA...")
+print("Create SLAE...")
 
 matrix = None
 right_side = None
@@ -85,12 +103,34 @@ for boundary in boundary_conditions:
             right_side = np.array([right_side_value])
         else:
             right_side = np.append(right_side, [right_side_value])
-        
 
-print("Solving SLA...")
+
+print("Solving SLAE...")
+
+solution = np.linalg.solve(matrix, right_side)
 
 print("Setting data back...")
 
+for term in expression:
+    term.propagate_solution(solution)
+
 print("Visualizing data...")
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+x_min = min(np.min(boundary_condition.get_border_elements()[:, 0]) for boundary_condition in boundary_conditions)
+y_min = min(np.min(boundary_condition.get_border_elements()[:, 1]) for boundary_condition in boundary_conditions)
+x_max = min(np.max(boundary_condition.get_border_elements()[:, 0]) for boundary_condition in boundary_conditions)
+y_max = min(np.max(boundary_condition.get_border_elements()[:, 1]) for boundary_condition in boundary_conditions)
+
+x_data = np.linspace(x_min, x_max, CHART_STEPS)
+y_data = np.linspace(y_min, y_max, CHART_STEPS)
+
+solution_func = lambda a, b: sum(term.get_value(a, b) for term in expression)
+
+surf = ax.plot_surface(x_data, y_data, solution_func(X, Y), cmap=cm.coolwarm, linewidth=0)
+
+plt.show()
+
 
 print("Done!")
